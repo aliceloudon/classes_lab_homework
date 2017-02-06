7ccb2f92ba462c66c6c9802c44c9d32bb39ee23f
Alice Loudon <alice_loudon@icloud.com>
2017-02-06 22:04:33 +0000

    all tests pass

--- classes_lab.rb
+++ classes_lab.rb
@@ -5,8 +5,38 @@ class Student
     @cohort = cohort
   end
 
+  def student_says()
+    return "I can talk!"
+  end
 
-
+  def favourite_language(language)
+    return "I love #{language}"
+  end
 
 end
 
+class SportsTeam
+  attr_accessor :team_name, :players, :coach, :points
+  def initialize(team_name, players, coach, points)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+    @points = points
+  end
+
+  def add_new_player(new_player)
+    @players << new_player
+    return @players
+  end
+
+  def player_present(name)
+    return players.include?(name)
+  end
+
+  def win_or_lose(result)
+    if result == "win"
+      @points += 1
+    end
+  end
+
+end
--- classes_lab_spec.rb
+++ classes_lab_spec.rb
@@ -6,13 +6,13 @@ require_relative('classes_lab.rb')
 class TestStudent < MiniTest::Test
 
   def test_student_name
-    person = Student.new("Alice", 10)
-    assert_equal(person.name, "Alice")
+    lisa = Student.new("Lisa", 10)
+    assert_equal("Lisa", lisa.name)
   end
 
   def test_cohort
-    person = Student.new("Adam", 10)
-    assert_equal(10, person.cohort)
+    steve = Student.new("Steve", 11)
+    assert_equal(11, steve.cohort)
   end
 
   def test_set_name
@@ -22,11 +22,60 @@ class TestStudent < MiniTest::Test
   end
 
   def test_set_cohort
-    person = Student.new("Adam", 10)
-    person.cohort = 11
-    assert_equal(11, person.cohort)
+    person = Student.new("Adam", 11)
+    person.cohort = 9
+    assert_equal(9, person.cohort)
   end
 
+  def test_student_says
+    alice = Student.new("Alice", 10)
+    assert_equal("I can talk!", alice.student_says)
+  end
+
+  def test_favourite_language
+    andrew = Student.new("Andrew", 5)
+    assert_equal("I love Ruby", andrew.favourite_language("Ruby"))
+  end
 
 end
 
+
+class TestSportsTeam < MiniTest::Test
+
+  def test_team_name
+    red = SportsTeam.new("Red", ["Sam", "Pete"], "Simon Knight", 2)
+    assert_equal("Red", red.team_name)
+  end
+
+  def test_team_players
+    blue = SportsTeam.new("Blue", ["Hannah", "Ian"], "Chris Grey", 3)
+    assert_equal(["Hannah", "Ian"], blue.players)
+  end
+
+  def test_team_coach
+    green = SportsTeam.new("Green", ["Emily"], "Michael Philips", 6)
+    assert_equal("Michael Philips", green.coach)
+  end
+
+  def test_change_coach
+    yellow = SportsTeam.new("Yellow", ["Joe", "Jo"], "Tom Fey", 1)
+    yellow.coach = "Steve Smith"
+    assert_equal("Steve Smith", yellow.coach)
+  end
+
+  def test_add_player
+    purple = SportsTeam.new("Purple", ["Craig"], "Tina Mitchell", 5)
+    assert_equal(["Craig", "Stuart"], purple.add_new_player("Stuart"))
+  end
+
+  def test_player_present
+    turquoise = SportsTeam.new("Turquoise", ["Sally", "Robert", "Becky", "Josh"], "Mike Richards", 5)
+    assert_equal(true, turquoise.player_present("Josh"))
+  end
+
+  def test_win_or_lose
+    orange = SportsTeam.new("Orange", ["Tim", "Tom"], "Gavin Whyte", 2)
+    assert_equal(3, orange.win_or_lose("win"))
+  end
+
+end
